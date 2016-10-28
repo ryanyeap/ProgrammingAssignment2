febb95ce2437f22023044671cc07c8121d640077
Ryan Yeap <ryanyeap@hotmail.com>
2016-10-28 09:10:52 +0800

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function stores a matrix with makeCacheMatrix()
+## ready to be 'used' by cacheSolve() function.
+## cacheSolve() returns the inverse of the matrix stored in
+## makeCacheMatrix()
 
-## Write a short comment describing this function
+## makeCacheMatrix():
+## Sets and gets of matrix input in m variable.
+## Sets and gets solve() in s variable.
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(m = matrix()) {
+	s <- NULL
+	set <- function(y) {
+		m <<- y
+		s <<- NULL
+	}
+	get <- function() m
+	setsolve <- function(solve) s <<- solve
+	getsolve <- function() s
+	list(set = set, get = get,
+		setsolve = setsolve,
+		getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve():
+## Function returns a inverse of the matrix provided
+## with solve()
 
-cacheSolve <- function(x, ...) {
+# Function first trys to get getsolve() and store in "s",
+# if result of "s" is NOT null; return a message and stored "s".
+
+# Otherwise get matrix with "m$get()" and store in "data".
+# Call function solve(), passing "data" and store in "s";
+# set s to global with m$setsolve(s)
+# finally returns "s"
+
+cacheSolve <- function(m, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <- m$getsolve()
+        if(!is.null(s)) {
+        	message("getting cached data")
+        	return(s)
+        }
+        data <- m$get()
+        s <- solve(data, ...)
+        m$setsolve(s)
+        s
 }
